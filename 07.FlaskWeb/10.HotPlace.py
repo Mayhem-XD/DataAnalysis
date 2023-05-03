21721a0814f41dd79d7a76582a73d05429af8245
Mayhem-XD <leahblo1994@gmail.com>
2023-05-03 10:42:11 +0900

    수정

--- 07.FlaskWeb/10.HotPlace.py
+++ 07.FlaskWeb/10.HotPlace.py
@@ -17,7 +17,7 @@ def hotplace():
         place2 = request.form['place2']
         place3 = request.form['place3']
         places = [place1, place2, place3]
-        mu.hot_places(places)
+        mu.hot_places(places, app)
         return render_template('10.HotPlaces_res.html')
 
 
