346c65b23e72631f42696179ee84e4d201f59665
Mayhem-XD <leahblo1994@gmail.com>
2023-07-05 12:32:08 +0900

    연습

---
+++ 09.MySQL/kpop_dao.py
@@ -0,0 +1,56 @@
+import json, pymysql
+with open('./mysql.json') as f:
+    config_str = f.read()
+config = json.loads(config_str)
+
+def get_song_list_by_debut(year):
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    sql ='''SELECT l.name, l.debut, r.title FROM girlgroup AS l JOIN song AS r ON l.hit_song_id = r.sid
+	    WHERE l.debut LIKE %s;'''
+    cur.execute(sql, (f"{year}%",))
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
+
+def insert_song(params):
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    sql ="INSERT INTO song VALUES(DEFAULT, %s, %s);"
+    cur.execute(sql,params)
+    conn.commit()       
+    cur.close()
+    conn.close
+    return None
+
+def insert_girl_group(params):
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    sql ="INSERT INTO girlgroup VALUES(DEFAULT, %s, %s, %s);"
+    cur.execute(sql,params)
+    conn.commit()       
+    cur.close()
+    conn.close
+    return None
+
+def get_song_list(num):
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    sql ='''SELECT title FROM song limit %s;'''
+    cur.execute(sql, num)
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
+
+def get_girl_group_list(num):
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    sql ='''SELECT l.name, l.debut FROM girlgroup AS l limit %s;'''
+    cur.execute(sql, num)
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
+
